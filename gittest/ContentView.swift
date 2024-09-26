15ea95321b3d6a8f9aeb1698481b40b144ed189f
muneerah <muneerah.aldhasy@gmail.com>
2024-09-26 17:06:00 +0300

    Initial Commit

---
+++ gittest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gittest
+//
+//  Created by muneerah aldhasi on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gittest/gittestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittestApp.swift
+//  gittest
+//
+//  Created by muneerah aldhasi on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct gittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
